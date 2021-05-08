ee07223f9e327b7a67fe739d11a542bd3a1bb44c
Filip Juza <filip.juza.2000@gmail.com>
2021-05-08 20:04:08 +0200

    add rest of procedures

--- DatabaseScripts/stored_procedures.sql
+++ DatabaseScripts/stored_procedures.sql
@@ -114,3 +114,65 @@ BEGIN
     end if;
     SET success = 1;
 end;
+
+
+
+create
+    definer = karolzaj@`%` procedure addQuestionToCategory(IN q_id int, IN cat_id int)
+BEGIN
+    INSERT INTO QuestionsSet(CategoryID, QuestionID) VALUES(cat_id, q_id);
+end;
+
+create
+    definer = karolzaj@`%` procedure createCategory(IN id int, IN name varchar(50))
+BEGIN
+    INSERT INTO Category(ClassID, Categoryname) VALUES(id, name);
+end;
+
+
+create
+    definer = karolzaj@`%` procedure createQuiz(IN Category_id int, IN title varchar(50), IN start_date datetime,
+                                                IN end_date datetime, IN q_count int)
+BEGIN
+    DECLARE quiz_id INT;
+    START TRANSACTION;
+    INSERT INTO Quiz(CategoryID, QuizTitle, StartDate, EndDate) VALUES
+    (Category_id, title, start_date, end_date);
+    SET quiz_id = LAST_INSERT_ID();
+    CALL pickQuizSet(quiz_id, Category_id, q_count, @result);
+    IF @result = 0 THEN
+        ROLLBACK;
+    ELSE
+        COMMIT;
+    end if;
+end;
+
+
+create
+    definer = karolzaj@`%` procedure loginUser(IN userEmail varchar(100), IN userPassword varchar(50), OUT id int)
+BEGIN
+    SELECT UserID INTO id FROM User
+        WHERE Password = userPassword AND Email = userEmail;
+end;
+
+
+create
+    definer = karolzaj@`%` procedure pickQuizSet(IN Quiz_id int, IN Category_id int, IN q_count int, OUT result int)
+BEGIN
+    DECLARE questionsInCategory INT;
+    SELECT COUNT(*) INTO questionsInCategory FROM QuestionsSet
+        WHERE CategoryID = Category_id;
+    IF questionsInCategory >= q_count THEN
+        INSERT INTO QuizSet(QuizID, QuestionID)  SELECT Quiz_id, QuestionID
+        FROM QuestionsSet
+        ORDER BY RAND()
+        LIMIT q_count;
+        SET result = 1;
+    ELSE
+        SET result = 0;
+    end if;
+end;
+
+
+
+
